f0169b0a5ae5f7b0a19741115a81acb45f5250c4
Ali Niko Kherad <alinikokherad@gmail.com>
2019-10-21 14:49:47 +0330

    wallet package create

--- Mentasystem/Wallet/src/WalletServiceProvider.php
+++ Mentasystem/Wallet/src/WalletServiceProvider.php
@@ -8,11 +8,11 @@ class WalletServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        include __DIR__ . '/routes/routes.php';
+        include __DIR__ . '/Routes/api.php';
     }
 
     public function register()
     {
-        $this->app->make('Mentasystem\Wallet\WalletController');
+//        $this->app->make('Mentasystem\Wallet\WalletController');
     }
 }
